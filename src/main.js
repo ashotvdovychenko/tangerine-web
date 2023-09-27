42819260ba4ca1b0028a7207e398927d6bbad164
Pagend <pariy.illya@gmail.com>
2023-09-27 15:40:24 +0300

    Add basic slider impl

--- src/main.js
+++ src/main.js
@@ -6,3 +6,26 @@ import '@fortawesome/fontawesome-free/js/all.min.js'
 import WOW from "wow.js/dist/wow.js";
 
 new WOW().init();
+
+import Swiper from 'swiper/bundle';
+
+import 'swiper/css/bundle';
+
+new Swiper('.dishes-slider', {
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false
+    },
+});
