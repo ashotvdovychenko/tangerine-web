139aeaaf52b6db475f77503c74df59301bf3599a
Pagend <68565486+Pagendera@users.noreply.github.com>
2023-09-27 17:32:25 +0300

    Implement main section with swiper (#20)
    * Add packages
    
    * Add basic slider impl
    
    * Add style for pagination bullets
    
    * Fix overlapping of swiper over nav-menu
    
    * Reorganized imports
    
    * Remove navigation buttons
    
    * Resize pagination bullets

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'wow.js/css/libs/animate.css';
 import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'swiper/css/bundle';
 
 import '@fortawesome/fontawesome-free/js/all.min.js'
+import Swiper from 'swiper/bundle';
 import WOW from "wow.js/dist/wow.js";
 
 new WOW().init();
+
+new Swiper('.dishes-slider', {
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false
+    },
+});
