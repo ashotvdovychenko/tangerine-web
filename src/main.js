705ac8562a5c919e4142ec72f804f938f2a39093
ashotvdovychenko <129285657+ashotvdovychenko@users.noreply.github.com>
2023-09-27 13:09:04 +0300

    Header & footer implementation (#15)
    * Add packages
    
    * Basic header and footer impl
    
    * Fix icons color
    
    * Styles to bootstrap refactor

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'wow.js/css/libs/animate.css';
+import '@fortawesome/fontawesome-free/css/all.min.css'
 
+import '@fortawesome/fontawesome-free/js/all.min.js'
 import WOW from "wow.js/dist/wow.js";
 
 new WOW().init();
