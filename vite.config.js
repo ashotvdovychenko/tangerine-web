2d2723d61b27a224edffeffc94bb555437b8f3e9
ashotvdovychenko <129285657+ashotvdovychenko@users.noreply.github.com>
2023-10-03 15:13:08 +0300

    Vite config for multipage app (#36)

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import path from 'path';
 export default {
     root: path.resolve(__dirname, 'src'),
     build: {
+        rollupOptions: {
+            input: {
+                main: path.resolve(__dirname, 'src/index.html'),
+                menuList: path.resolve(__dirname, 'src/menu-list/menu-list.html'),
+                receiptList: path.resolve(__dirname, 'src/receipt-list/receipt-list.html')
+            },
+        },
         outDir: '../dist'
     },
     server: {
